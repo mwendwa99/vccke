2a01b33a6308b5cde7b9645333aa8692782b2b6f
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2021-10-16 12:58:43 +0300

    Update Volunteer.jsx

--- src/components/Volunteer.jsx
+++ src/components/Volunteer.jsx
@@ -1,4 +1,4 @@
-import { Container, Fade, Grid, Typography, Divider, Button } from '@mui/material'
+import { Container, Fade, Grid, Typography, Button } from '@mui/material'
 import { Box } from '@mui/system'
 import { makeStyles } from '@mui/styles';
 import React from 'react';
