06f9888c8b36e8f7495da0c108989a580a3e9a1d
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2023-10-20 18:12:14 +0300

    remove unnexessary assets

---
+++ src/theme.js
@@ -0,0 +1,22 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  //   palette: {
+  //     primary: {
+  //       //   main: "#f44336",
+  //     },
+  //     secondary: {
+  //       //   main: "#3f51b5",
+  //     },
+  //   },
+  typography: {
+    fontFamily: "Lato, sans-serif",
+    h1: {
+      fontSize: "48px",
+      fontWeight: 700,
+      color: "#CD4D88",
+    },
+  },
+});
+
+export default theme;
